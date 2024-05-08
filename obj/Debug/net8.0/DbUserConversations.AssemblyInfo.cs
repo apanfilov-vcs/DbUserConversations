0b50102a0d2675233e6524be8eb323870cb18997
Artem Panfilov <apanfilov@vcstudent.org>
2024-05-07 19:00:57 -0700

    Updated object cache.

--- obj/Debug/net8.0/DbUserConversations.AssemblyInfo.cs
+++ obj/Debug/net8.0/DbUserConversations.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DbUserConversations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f66490c9b0f07514dbd0907d3c8217ccbe340dcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d13aa75f3d7d1d51df611653ef71731fe1cf5877")]
 [assembly: System.Reflection.AssemblyProductAttribute("DbUserConversations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DbUserConversations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
