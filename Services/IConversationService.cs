42e232159561df5e73fbe3a0d3ea5a350d140185
Artem Panfilov <apanfilov@vcstudent.org>
2024-05-07 18:17:17 -0700

    Updated AddConversation() parameters in IConversationService.cs.

--- Services/IConversationService.cs
+++ Services/IConversationService.cs
@@ -12,7 +12,7 @@ namespace DbUserConversations.Services
     {
         Task<ServiceResponse<List<GetConversationDto>>> GetConversations(ClaimsPrincipal claimsPrincipal);
         Task<ServiceResponse<GetConversationDto>> GetConversationWithMessagesById(ClaimsPrincipal claimsPrincipal, string id);
-        Task<ServiceResponse<GetConversationDto>> AddConversation(ClaimsPrincipal claimsPrincipal);
+        Task<ServiceResponse<GetConversationDto>> AddConversation(ClaimsPrincipal claimsPrincipal, string name);
         Task<ServiceResponse<GetConversationDto>> AddUserToConversationById(ClaimsPrincipal claimsPrincipal, string conversationId, string userId);
         Task<ServiceResponse<GetConversationDto>> UpdateConversationNameById(ClaimsPrincipal claimsPrincipal, string id, string name);
         Task<ServiceResponse<GetConversationDto>> RemoveUserFromConversationById(ClaimsPrincipal claimsPrincipal, string conversationId);
--- obj/Debug/net8.0/DbUserConversations.AssemblyInfo.cs
+++ obj/Debug/net8.0/DbUserConversations.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DbUserConversations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+557d06dcacd97ff45f0dc945f294e3c982763f7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e47bfaf3549d135aa66b7fc3dbaca7abe004a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DbUserConversations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DbUserConversations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
